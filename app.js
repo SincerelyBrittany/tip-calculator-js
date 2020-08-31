e70e7c5488e5ecc7a5d97a2f3a5b776bff066d6f
SincerelyBrittany <sincerelybr@gmail.com>
2020-08-31 14:25:36 -0400

    converts tipAmount and checkAmount values to integers

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ let form = document.querySelector('form')
 
 form.addEventListener("click", function(e){
     console.log(e.currentTarget)
-    let checkAmount = document.getElementById('check-amount')
-    let tipAmount = document.getElementById('sel1')
-    console.log(checkAmount.value)
-    console.log(tipAmount.value)
+    let checkAmount = +document.getElementById('check-amount').value
+    let tipAmount = +document.getElementById('sel1').value
+    // console.log(tipAmount.options[e.selectedIndex])
+    console.log(checkAmount)
+    console.log(tipAmount)
 });
