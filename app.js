8364dd405a9960fa18c9e2308cc9565542e366b6
SincerelyBrittany <sincerelybr@gmail.com>
2020-08-31 14:49:33 -0400

    calculates the results and renders to page

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 // console.log("hello")
 function calculate(total, tip){
-    let calculation = ((total * tip)/100)
-    console.log(calculation)
-    let container = document.querySelector('.container1')
-    let div = document.createElement('div')
-    div.innerHTML = `<h3>${calculation}</h3>`;
-    container.append(div)
+    let calculation = (((total * tip)/100).toFixed(2))
+    displayResults(calculation)
+}
+
+function displayResults(totalAmount){
+    // console.log(totalAmount)
+    let span = document.getElementById('total')
+    span.innerHTML = `${totalAmount}`
+    // let container = document.querySelector('.container1')
+    // let div = document.createElement('div')
+    // div.innerHTML = `<h3>${totalAmount}</h3>`;
+    // container.append(div)
 }
 
 let form = document.querySelector('form')
 
 form.addEventListener("click", function(e){
+    e.preventDefault()
     console.log(e.currentTarget)
     let checkAmount = +document.getElementById('check-amount').value;
     let tipAmount = +document.getElementById('sel1').value;
