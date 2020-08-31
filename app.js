25c0d11bb535e79750d162e2cc0ca609d89b8aa8
SincerelyBrittany <sincerelybr@gmail.com>
2020-08-31 14:26:24 -0400

    creating tip amount calculation function

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ let form = document.querySelector('form')
 
 form.addEventListener("click", function(e){
     console.log(e.currentTarget)
-    let checkAmount = +document.getElementById('check-amount').value
-    let tipAmount = +document.getElementById('sel1').value
+    let checkAmount = +document.getElementById('check-amount').value;
+    let tipAmount = +document.getElementById('sel1').value;
     // console.log(tipAmount.options[e.selectedIndex])
     console.log(checkAmount)
     console.log(tipAmount)
+    calculate();
 });
