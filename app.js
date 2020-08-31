d95d50cdd0853d58ebe487ae4e9055654b170f9b
SincerelyBrittany <sincerelybr@gmail.com>
2020-08-31 14:30:12 -0400

    creates tip calculation

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 // console.log("hello")
+function calculate(total, tip){
+    let calculation = ((total * tip)/100)
+    console.log(calculation)
+    
+}
 
 let form = document.querySelector('form')
 
@@ -7,7 +12,7 @@ form.addEventListener("click", function(e){
     let checkAmount = +document.getElementById('check-amount').value;
     let tipAmount = +document.getElementById('sel1').value;
     // console.log(tipAmount.options[e.selectedIndex])
-    console.log(checkAmount)
-    console.log(tipAmount)
-    calculate();
+    // console.log(checkAmount)
+    // console.log(tipAmount)
+    calculate(checkAmount, tipAmount);
 });
