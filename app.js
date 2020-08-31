4d1cf90c88c1553e8fa049e9f07473e13e5a3ee1
SincerelyBrittany <sincerelybr@gmail.com>
2020-08-31 14:06:25 -0400

    gets the check amount

--- app.js
+++ app.js
@@ -3,5 +3,7 @@
 let form = document.querySelector('form')
 
 form.addEventListener("click", function(e){
-    console.log(e)
+    console.log(e.currentTarget.value)
+    let checkAmount = document.getElementById('check-amount')
+    console.log(checkAmount.value)
 });
